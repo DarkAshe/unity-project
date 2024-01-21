526c1b6fc16008b495ca861b9b83acec2b81013c
Nuggiez <sirnicholas975@gmail.com>
2024-01-21 18:43:31 +0200

    f

---
+++ unity-project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class GameManager : MonoBehaviour {
+    
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 6;
+
+        void Awake()
+        {
+            if(instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+            boardScript = GetComponent<BoardManager>();
+            InitGame();
+        }
+
+        void InitGame()
+        {
+            boardScript.SetupScene(level);
+        }
+    
+
+        void Update() {
+            
+    }
+}
